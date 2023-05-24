202a0b9d610d13e1142f2be445bd58251a474297
Natia Lollie <natialollie.developer@gmail.com>
2023-05-24 16:31:01 -0500

    chore: gulp push to dist for deployment re-test

--- dist/js/component.js
+++ dist/js/component.js
@@ -1,19 +1,18 @@
-class Volunteer extends HTMLElement{connectedCallback(){this.innerHTML=`
-        <div class="volunteer-wrapper">
-            <div class="volunteer-card">
-                <a target="_blank" href="https://www.womenwhocode.com/"><img class="card-image"
-                    src="_imgs/thumbnails/volunteer/women-who-code1.png" alt="Women Who Code"></a>
+class Award extends HTMLElement{connectedCallback(){this.innerHTML=`
+        <div class="award-wrapper">
+        <div class="award-card">
+                <a target="_blank" href="https://girldevelopit.com/programs/hackathon-showcase/"><img class="card-image"
+                    src="_imgs/thumbnails/contribution/gdi-hackathon.png" alt="GDI Hackathon 2nd Place Winner"></a>
+                     <figcaption style="font-size: 12px;text-align:center!important;">GDI: 2nd Place Winner</figcaption>
             </div>
-            <div class="volunteer-card">
+        <div class="award-card">
                 <figure>
-                <a target="_blank" href="https://sic.edc.org/"> <img class="card-image" 
-                    src="_imgs/thumbnails/volunteer/samsung-ai-ml.svg" alt="Samsung Innovation Campus"> </a>
-                <figcaption style="font-style: italic;font-size: 12px;text-align:center!important;">Innovation Campus</figcaption>
+                <a target="_blank" href="https://news.samsung.com/us/samsung-innovation-campus-learner-spotlight-natia-lollie/?utm_source=linkedin&utm_medium=usnews&utm_campaign=general"> <img class="card-image" 
+                    src="_imgs/thumbnails/contribution/samsung-badge.png" alt="Samsung Innovation Campus Featured Student"></a>
+                <figcaption style="font-size: 12px;text-align:center!important;">Samsung: Featured Student</figcaption>
                 </figure>
-            
-                    <!-- <a target="_blank" href="https://seattlegivecamp.org/"> <img class="card-image" 
-                src="_imgs/thumbnails/volunteer/seattle-give-camp1.png" alt="Seattle Give Camp"> </a> -->
             </div>
+
         </div>
         `}}class Rights extends HTMLElement{connectedCallback(){this.innerHTML=`
         <div class="footer-rights">
@@ -21,4 +20,4 @@ class Volunteer extends HTMLElement{connectedCallback(){this.innerHTML=`
             <!--<p>Portfolio designed and developed by Natia Lollie | github.com/natiaLollie</p>-->
             <p class="mobile-resume" style="font-size: 12px; padding-top: 10px;"><a target="_blank" href="#"><em>Download Resume</em></a></p>
         </div>
-        `}}customElements.define("volunteer-icons",Volunteer),customElements.define("footer-rights",Rights);
+        `}}customElements.define("award-icons",Award),customElements.define("footer-rights",Rights);
